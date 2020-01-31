c2760bd18a7e2fa665d9d7efef13066cd36b401c
rjedwards105 <54290212+rjedwards105@users.noreply.github.com>
2020-01-30 22:55:03 -0800

    done
    DONE

---
+++ Message.java
@@ -0,0 +1,13 @@
+public class Messages
+{
+    public static void main (String[] args)
+    {
+        Thought thinking = new Thought();
+        Advice speaking = new Advice();
+
+        thinking.messages();
+
+        speaking.messages();
+    }
+
+}
